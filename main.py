c258b71999f4a8b0d7a0a40c8bce54e792eef1ad
a1j9o94 <obletonadrian@gmail.com>
2023-05-10 03:13:49 +0000

    I was able to call myself and have conversation with AI

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import Flask and other libraries
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, request
 from twilio.twiml.voice_response import VoiceResponse
 import os
 # Import the Twilio and Eleven Labs libraries
@@ -23,26 +23,38 @@ webhook_url = "http://ai-phone-bank-poc.a1j9o94.repl.co/twilio"
 # Create a Twilio client object
 client = Client(account_sid, auth_token)
 
+# GPT API System Prompts
+system_prompt = "You are a campaign worker trying to convince someone to go vote, and vote for your preferred candidate. You speak casually, and try not to overwhelm people with information. You're responses should be concicse, informative, and understanding. You ask lots of questions to understand what is important to each voter. Begin by asking the voter if they are aware of the up coming election. Do not reveal that you are an AI bot."
+
+conversation = [{"role": "system", "content": system_prompt}]
+
 
 # Define a route for handling Twilio webhook requests
 @app.route("/twilio", methods=['POST'])
 def twilio():
+	"""Check if the request has a gather response. If it does, then gather the speech, get the response from the AI, and send it back to the user.""" ""
+	# Parse the Twilio request
+	speech_result = request.values.get('SpeechResult', None)
 	response = VoiceResponse()
-	completion = openai.ChatCompletion.create(
-	 model="gpt-3.5-turbo",
-	 messages=[
-	  {
-	   "role":
-	   "system",
-	   "content":
-	   "You are a campaign worker trying to convince someone to go vote, and vote for your preferred candidate."
-	  },
-	 ],
-	 temperature=0.7)
+
+	if speech_result:
+		# Parse the gather response
+		print(speech_result)
+		user_response = {"role": "user", "content": speech_result}
+		conversation.append(user_response)
+		completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+		                                          messages=conversation,
+		                                          temperature=0.7)
+
+	else:
+		completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+		                                          messages=conversation,
+		                                          temperature=0.7)
 	text = completion.choices[0].message.content
-	response.say(text)
-	response.say(completion.choices[0].message.content)
+	conversation.append(completion.choices[0].message)
 	# Return the response as XML
+	response.say(text)
+	response.gather(input="speech", action=webhook_url, method="POST")
 	print(response.to_xml())
 	return Response(response.to_xml(), content_type="text/xml")
 
@@ -61,7 +73,7 @@ def call():
 	print(call)
 
 	# Return a TwiML response
-	return render_template('home.html')
+	return render_template('home.html', result="Made Call")
 
 
 #Run the app on port 5000
