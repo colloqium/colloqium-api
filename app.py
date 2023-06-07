d1de648a6abb0c2ea804be8d924cc94c68d647bb
Adrian Obleton <obletonadrian@gmail.com>
2023-06-07 12:09:20 -0700

    Got running on my local machine

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask_wtf.csrf import CSRFProtect
 from flask_migrate import Migrate
 from database import db
 import secrets
+from dotenv import load_dotenv
 
 
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
     app.config['SECRET_KEY'] = secrets.token_hex(nbytes=8)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
