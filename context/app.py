3fca113e58eb963612c94cb3dc04e66fe1db9594
Adrian Obleton <obletonadrian@gmail.com>
2023-06-15 13:00:16 -0400

    Checking to see if the database exist before running and deleting it if it does

--- context/app.py
+++ context/app.py
@@ -5,6 +5,7 @@ from context.database import db
 import secrets
 from dotenv import load_dotenv
 from routes.blueprint import bp
+import os
 
 
 def create_app():
@@ -13,11 +14,14 @@ def create_app():
     app.register_blueprint(bp)
     app.config['SECRET_KEY'] = secrets.token_hex(nbytes=8)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    db_path = os.path.join(os.getcwd(), '../instance/site.db')
 
     db.init_app(app)
 
     with app.app_context():
-        db.create_all()  # Create all tables
+        if os.path.exists(db_path):  # check if db file exists
+            os.remove(db_path)  # if it does, remove it
+        db.create_all()  # then create all tables
         Migrate(app, db)  # initialize Flask-Migrate
 
     return app
