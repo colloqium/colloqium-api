a126350b0f8f5118a2a0b01d3cd308cc295102f3
Adrian Obleton <obletonadrian@gmail.com>
2023-07-13 19:55:23 -0400

    bugfix: Removed test numbers

--- tools/init_db.py
+++ tools/init_db.py
@@ -1,4 +1,5 @@
 from context.database import db
+from models.models import Recipient, Sender, Interaction, Audience, Campaign, PhoneNumber
 from flask import Flask
 from dotenv import load_dotenv
 import secrets
@@ -12,10 +13,12 @@ def init_db():
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
     db.init_app(app)
+
+
     with app.app_context():
-        if db.engine.table_names():
+        if db.inspect(db.engine).get_table_names():
             db.drop_all()
-            db.create_all()
+        db.create_all()
 
 if __name__ == "__main__":
     init_db()
