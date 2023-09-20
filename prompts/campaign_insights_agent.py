3bc3f9c3c60bf4efea5a561a77e562c6f9af4c26
Adrian Obleton <obletonadrian@gmail.com>
2023-09-20 17:15:08 -0400

    chore: Only generating insights for interactions that have gotten a response.

--- prompts/campaign_insights_agent.py
+++ prompts/campaign_insights_agent.py
@@ -1,12 +1,14 @@
 from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate
 from models.sender import Campaign, Sender
+from models.interaction import Interaction, InteractionStatus
 
 
 def get_campaign_summary_system_prompt(campaign: Campaign):
 
     # Get an array of all the summary information from each interaction in this campaign
     # relevant fields are: goal_achieved, rating_explanation, rating, campaign_relevance_score, campaign_relevance_explanation, campaign_relevance_summary, insights_about_issues, insights_about_voter
-    interactions = campaign.interactions
+    # filter out interaction statuses that are less than responded
+    interactions = Interaction.query.filter(Interaction.campaign_id == campaign.id, Interaction.interaction_status >= InteractionStatus.RESPONDED).all()
     sender = Sender.query.get(campaign.sender_id)
 
     # Get the relevant fields for all interactions and append them to an array
@@ -38,7 +40,7 @@ def get_campaign_summary_system_prompt(campaign: Campaign):
                 Your team has just completed a targeted voter outreach. Your role is to synthesize this data into actionable, brief summaries for different roles on the campaign team.
 
                 You will generate:
-                1. Policy insights focusing on 1) the most common and 2) the most interesting policy areas. Limit to 5 areas or fewer.
+                1. Policy insights focusing on 1) the most common and 2) the most interesting policy areas. Limit to 5 areas or fewer. Leave empty if no insights specifically from this campaign.
                 2. A 15-20 word summary for the Communications Director.
                 3. A 15-20 word summary for the Field Director.
                 4. A 15-20 word summary for the Campaign Manager, synthesizing all the above.
