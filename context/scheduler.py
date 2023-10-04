d256895fd28be11ad800f33b9b7e6fc7b0372e4d
Adrian Obleton <obletonadrian@gmail.com>
2023-10-03 23:43:15 -0400

    feat: Basic agents with function calls implemented

--- context/scheduler.py
+++ context/scheduler.py
@@ -8,6 +8,6 @@ def job_listener(event):
     else:
         print("Job executed successfully")
 
-scheduler = BackgroundScheduler() #GeventScheduler()
+scheduler = GeventScheduler() #BackgroundScheduler()
 
 scheduler.add_listener(job_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
